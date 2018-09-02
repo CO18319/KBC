683b8a274b17533fd810f8fcb756612cde7feead
CO18319 <deepindersingh653@gmail.com>
2018-09-02 14:37:50 +0530

    Add Existing File

---
+++ kbc.c
@@ -0,0 +1,422 @@
+﻿/*Kaun Banega Crorepati*/
+#include<stdio.h>
+int main()
+{
+
+int i,x;
+
+   printf("Welcome to Kaun Banega Crorepati\n");
+   printf("Rules of the Game are\nThere will be fifteen questions which will be in series of multiple choice questions on their way to win top cash prize.\n");
+   printf("So lets start with the game.\n\n");
+   printf("Your first question on your computer screen.\n\nQ1. Who was the first president of India?\n1.Jawaharlal Nehru     2.Mahatma Gandhi    3.Dr. Rajendra Prasad    4.Subhash Chandra Bose\n\n");
+   printf("You want to use a lifeline?\n");
+   printf("1.Audience poll\n2.50-50\n3.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)20 (2)10 (3)60 (4)10\nAudience is with option 3\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("1.Jawaharlal Nehru   3.Dr. Rajendra Prasad\n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i);
+ if(i==3)
+{
+   printf("You are absolutely Right , You win Rs.1000\n\n");
+   printf("Your second question on your computer screen\n\nQ2. Who is popularly known as 'Father of Indian Constitution'?\n1.Jawaharlal Nehru    2.B.R.Ambedkar   3.Mahatma Gandhi    4.Bal Gangadhar Tilak\n");
+   printf("You want to use a lifeline?\n");
+   printf("1.Audience poll\n2.50-50\n3.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)15 (2)70 (3)10 (4)5\nAudience is with option 2\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("2.B.R.Ambedkar   3.Mahatma Gandhi\n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}
+    scanf("%d",&i);
+ if(i==2)
+ {
+   printf("You are absolutely Right , You win Rs.2000\n\n");
+   printf("Your third question on your computer screen\n\nQ3. Who is the father of computer?\n1.Dennis Ritchie     2.Larry page    3.Sergey Brin    4.Charles Babbage\n");
+   printf("You want to use a lifeline?\n");
+   printf("1.Audience poll\n2.50-50\n3.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)10 (2)7 (3)3 (4)80\nAudience is with option 4\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("1.Dennis Ritchie   4.Charles Babbage \n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i); 
+ if(i==4)
+ {
+   printf("You are absolutely Right , You win Rs.3000\n\n");
+   printf("Your fourth question on your computer screen\n\nQ4. Where did India play its 1st one day international match?\n1.Lords   2.Headingley   3.Taunton     4.The Oval\n");
+   printf("You want to use a lifeline?\n");
+   printf("1.Audience poll\n2.50-50\n3.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)30 (2)50 (3)10 (4)10\nAudience is with option 2\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("1.Lords   2.Headingley\n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i); 
+ if(i==2)
+ {
+   printf("You are absolutely Right , You win Rs.5000\n\n");
+   printf("Your fifth question on your computer screen\n\nQ5. Where did the 1st ODI match was played in India?\n1.New Delhi   2.Kolkata   3.Ahmedabad    4.Mumbai\n");
+   printf("You want to use a lifeline?\n");
+  printf("1.Audience poll\n2.50-50\n3.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)20 (2)10 (3)60 (4)10\nAudience is with option 3\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("2.Kolkata   3.Ahmedabad  \n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i); 
+ if(i==3)
+{
+   printf("You are absolutely Right , You win Rs.10000\n\n");
+   printf("Your sixth question on your computer screen\n\nQ6. How many players of Kho-Kho team can play on the field during the match?\n1.10   2.9   3.7    4.8\n");
+   printf("You want to use a lifeline?\n");
+  printf("1.Audience poll\n2.50-50\n3.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)10 (2)40 (3)20 (4)30\nAudience is with option 2\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("2.9   4.8\n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i); 
+ if(i==2)
+{
+   printf("You are absolutely Right , You win Rs.20000\n\n");
+   printf("Your seventh question on your computer screen\n\nQ7. Who among the following was India's longest serving Prime Minister?\n1.Indira Gandhi   2.Jawaharlal Nehru   3.Lal Bahadur Shastri  4.V.P.Singh \n");
+   printf("You want to use a lifeline?\n");
+  printf("1.Audience poll\n2.50-50\n3.None\n");
+  scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)0 (2)60 (3)10 (4)10\nAudience is with option 2\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("1.Indira Gandhi   2.Jawaharlal Nehru\n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i); 
+ if(i==2)
+{
+   printf("You are absolutely Right , You win Rs.40000\n\n");
+   printf("Your eighth question on your computer screen\n\nQ8. C programs are converted into machine language with the help of __________\n1.A compiler    2.An Editor   3.An operating system   4.None of these\n");
+   printf("You want to use a lifeline?\n");
+   printf("1.Audience poll\n2.50-50\n3.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)50 (2)30 (3)10 (4)10\nAudience is with option 1\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("1.A compiler     3.An operating system \n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i); 
+ if(i==1)
+{
+   printf("You are absolutely Right , You win Rs.80000\n\n");
+   printf("Your ninth question on your computer screen\n\nQ9. Hierarchy of operators in C language decides which operator_________?\n1.is most important   2.is used first  3.is fastest    4.operates on    largest numbers\n");
+   printf("You want to use a lifeline?\n");
+   printf("1.Audience poll\n2.50-50\n3.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)10 (2)60 (3)20 (4)10\nAudience is with option 2\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("2.is used first  4.operates on largest numbers\n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i); 
+ if(i==2)
+{
+   printf("You are absolutely Right , You win Rs.160000\n\n");
+   printf("Your tenth question on your computer screen\n\nQ10. The International Literacy Day is observed on?\n1.Sep 22   2.Nov 28   3.May 2    4.Sep 8\n");
+   printf("You want to use a lifeline?\n");
+   printf("1.Audience poll\n2.50-50\n3.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)10 (2)20 (3)20 (4)50\nAudience is with option 4\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("1.Sep 22    4.Sep 8\n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i); 
+ if(i==4)
+{
+   printf("You are absolutely Right , You win Rs.320000\n\n");
+   printf("Your eleventh question on your computer screen\n\nQ11. Name the oldest mountain range of India...?\n1.Himalayas   2.Nilgiris   3.Aravallis    4.Vindhyas\n");
+   printf("You want to use a lifeline?\n");
+   printf("a.Audience poll\nb.50-50\nc.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)20 (2)10 (3)60 (4)10\nAudience is with option 3\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("2.Nilgiris   3.Aravallis\n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i); 
+ if(i==3)
+{
+   printf("You are absolutely Right , You win Rs.640000\n\n");
+   printf("Your twelth question on your computer screen\n\nQ12. What is the name of new capital of Andhra Pradesh?\n1.Vishakhapatnam   2.Amravati   3.Kurnool    4.Hyderabad\n");
+   printf("You want to use a lifeline?\n");
+   printf("1.Audience poll\n2.50-50\n3.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)5 (2)60 (3)5 (4)30\nAudience is with option 2\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("2.Amravati 4.Hyderabad\n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i); 
+ if(i==2)
+{
+   printf("You are absolutely Right , You win Rs.1250000\n\n");
+   printf("Your thirteenth question on your computer screen\n\nQ13.  The Indian football team made its first appearance at Olympics in...?\n1.1936   2.1952   3.1948    4.1956\n");
+   printf("You want to use a lifeline?\n");
+   printf("a.Audience poll\nb.50-50\nc.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 'a':
+    printf("(1)20 (2)10 (3)60 (4)10\nAudience is with option 3\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("2.1952   3.1948\n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i); 
+ if(i==3)
+{
+   printf("You are absolutely Right , You win Rs.2500000\n\n");
+   printf("Your fourteenth question on your computer screen\n\nQ14. What was the name given to the supercluster of galaxies, discovered by ateam of Indian astronomers in 2017?\n1.Laxmi   2.Parvati  3.Saraswati   4.Durga \n");
+   printf("You want to use a lifeline?\n");
+   printf("1.Audience poll\n2.50-50\n3.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)20 (2)10 (3)60 (4)10\nAudience is with option 3\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("3.Saraswati   4.Durga\n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i); 
+ if(i==3)
+{
+   printf("You are absolutely Right , You win Rs.5000000\n\n");
+   printf("Your fifteenth question on your computer screen\n\nQ15. Who, in 1978, became the first person to be born in the continent of Antarctica?\n1.Emilio Palma  2.James Weddell    3.James William     4.Charles Wilkes\n");
+   printf("You want to use a lifeline?\n");
+  printf("1.Audience poll\n2.50-50\n3.None\n");
+   scanf("%d",&x);
+  
+switch(x)
+{
+case 1:
+    printf("(1)40 (2)30 (3)10 (4)20\nAudience is with option 1\nWhat do you want to do?\n");
+    break;
+case 2:
+    printf("1.Emilio Palma  4.Charles Wilkes\n");
+    break;
+case 3:
+    printf("Ok , then please give your answer\n");
+default:
+    printf("(No lifeline used)\n");
+
+}   
+    scanf("%d",&i); 
+ if(i==1)
+{
+   printf("Congratulations! You are absolutely right. You win Rs.1 Crore\n");
+
+}
+
+else
+printf("You are Wrong , you only win Rs.640000.\n");
+}
+
+else
+printf("You are Wrong , you only win Rs.640000.\n");
+}
+
+else
+printf("You are Wrong , you only win Rs.640000.\n");
+}
+
+else
+printf("You are Wrong , you only win Rs.640000.\n");
+}
+
+else
+printf("You are Wrong , you only win Rs.40000.\n");
+}
+
+else
+printf("You are Wrong , you only win Rs.40000.\n");
+}
+
+else
+printf("You are Wrong , you only win Rs.40000.\n");
+}
+
+else
+printf("You are Wrong , you only win Rs.40000.\n");
+}
+
+else
+printf("You are Wrong , you only win Rs.5000.\n");
+}
+
+else
+printf("You are Wrong , you only win Rs.5000.\n");
+}
+
+else
+printf("You are Wrong , you only win Rs.5000.\n");
+}
+
+
+else
+printf("You are Wrong , you win nothing.\n");
+}
+
+else
+printf("You are Wrong , you win nothing.\n");
+}
+
+else
+printf("You are Wrong , you win nothing.\n");
+}
+
+else
+printf("You are Wrong , you win nothing.\n");
+
+
+
+return 0;
+} 
